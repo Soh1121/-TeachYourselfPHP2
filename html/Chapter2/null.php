18d2a4af091fa36c7021b12dcc5052ac7a21b0d5
Soh1121 <satou.shg@gmail.com>
2021-10-24 17:22:30 +0900

    [add]nullの定義方法を確認

---
+++ html/Chapter2/null.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>PHPの基本</title>
+</head>
+
+<body>
+	<?php
+	$n1;	// 値が代入されていないのでnull
+	$n2 = null;
+	print $n1;
+	print $n2;
+	?>
+</body>
+
+</html>
