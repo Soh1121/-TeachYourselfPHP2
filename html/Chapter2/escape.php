c877839bd52ca1cde71a251d36e69d4d78055a73
Soh1121 <satou.shg@gmail.com>
2021-10-24 17:12:02 +0900

    [add]エスケープ処理を確認

---
+++ html/Chapter2/escape.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>PHPの基本</title>
+</head>
+
+<body>
+	<?php
+	$str = 'He\'s "GREAT" teacher.';
+	print $str;	// 結果：He's "GREAT" teacher.
+	?>
+</body>
+
+</html>
