4e2a133711a9eeb3e17d58c1069baf4f90229c3a
Soh1121 <satou.shg@gmail.com>
2021-10-30 16:06:20 +0900

    [add]通常配列と連想配列の関係を学習

---
+++ html/Chapter2/array_index1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>PHPの基本</title>
+</head>
+
+<body>
+	<?php
+	$data = [1 => '山田', 2 => '掛谷', 3 => '日尾', 4 => '本多'];
+	print_r($data);
+	?>
+</body>
+
+</html>
---
+++ html/Chapter2/array_type.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>PHPの基本</title>
+</head>
+
+<body>
+	<?php
+	$data = [
+		'str' => 0,		// 文字列
+		'7' => 1,			// 整数
+		'07' => 2, 		// 整数（のように見えるが…）
+		10.5 => 3,		// 小数点数
+		true => 4,		// bool値
+		null => 5,		// null値
+	];
+	print_r($data);
+	?>
+</body>
+
+</html>
