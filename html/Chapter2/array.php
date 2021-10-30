21f68416101caf2e99b01d5f78e1d699314b6ade
Soh1121 <satou.shg@gmail.com>
2021-10-30 15:43:23 +0900

    [add]配列の基本を学習

---
+++ html/Chapter2/array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>PHPの基本</title>
+</head>
+
+<body>
+	<?php
+	$data = ['山田', '掛谷', '日尾', '本多', '矢吹'];
+	print $data[3];
+	?>
+</body>
+
+</html>
