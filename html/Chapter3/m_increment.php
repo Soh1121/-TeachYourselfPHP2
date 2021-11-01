4540bc3c6dfbaac7962fe0324c5896cea7a15018
Soh1121 <satou.shg@gmail.com>
2021-11-01 22:04:36 +0900

    [add]マジカルインクリメントを学習

---
+++ html/Chapter3/m_increment.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>演算子</title>
+</head>
+
+<body>
+	<?php
+	$i = 'Z';
+	print ++$i . "\n";
+	print ++$i . "\n";
+
+	$j = 'T8';
+	print ++$j . "\n";
+	print ++$j . "\n";
+	?>
+</body>
+
+</html>
