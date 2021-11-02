f93c3970121344fee8f6b8494913338a82844f87
Soh1121 <satou.shg@gmail.com>
2021-11-02 11:10:10 +0900

    [add]厳密な等価演算子を学習

--- html/Chapter3/equal.php
+++ html/Chapter3/equal.php
@@ -8,16 +8,16 @@
 
 <body>
 	<?php
-	var_dump('3.14' == 3.1400000);
-	var_dump('3.14E2' == 314);
-	var_dump('0x10' == 16);
-	var_dump('010' == 8);
-	var_dump('0b11' == 3);
-	var_dump('13xyz' == 13);
-	var_dump('X' == 0);
-	var_dump('3.14' == '3.1400000');
-	var_dump('3.14E2' == '314');
-	var_dump('13xyz' == '13');
+	var_dump('3.14' == 3.1400000);		// 結果︰bool(true)
+	var_dump('3.14E2' == 314);				// 結果︰bool(true)
+	var_dump('0x10' == 16);						// 結果︰bool(false)
+	var_dump('010' == 8);							// 結果︰bool(false)
+	var_dump('0b11' == 3);						// 結果︰bool(false)
+	var_dump('13xyz' == 13);					// 結果︰bool(true)
+	var_dump('X' == 0);								// 結果︰bool(true)
+	var_dump('3.14' == '3.1400000');	// 結果︰bool(true)
+	var_dump('3.14E2' == '314');			// 結果︰bool(true)
+	var_dump('13xyz' == '13');				// 結果︰bool(false)
 	?>
 </body>
 
