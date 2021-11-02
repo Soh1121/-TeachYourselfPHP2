66c4b6c9ba75c4b46a8d03c526dc7388ed2322bc
Soh1121 <satou.shg@gmail.com>
2021-11-02 11:26:03 +0900

    [add]条件演算子を学習

---
+++ html/Chapter3/condition1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>演算子</title>
+</head>
+
+<body>
+	<?php
+	$score = 75;
+	print $score >= 70 ? '合格！' : '不合格...';
+	?>
+</body>
+
+</html>
