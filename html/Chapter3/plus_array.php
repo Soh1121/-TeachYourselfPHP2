42b8b7911ad71171c6905cfae91b540c0a8d7d75
Soh1121 <satou.shg@gmail.com>
2021-11-02 10:59:02 +0900

    [add]値による代入と参照による代入を学習

---
+++ html/Chapter3/plus_array.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>演算子</title>
+</head>
+
+<body>
+	<?php
+	$assoc1 = [
+		'Apple' => 'Red',
+		'Orange' => 'Yellow',
+		'Melon' => 'Green'
+	];
+	$assoc2 = [
+		'Grape' => 'Purple',
+		'Apple' => 'Green',
+		'Strawberry' => 'Red'
+	];
+	$result = $assoc1 + $assoc2;
+	print_r($result);
+
+	$ary1 = [1, 3, 5];
+	$ary2 = [2, 3, 6];
+	$result = $ary1 + $ary2;
+	print_r($result);
+	?>
+</body>
+
+</html>
---
+++ html/Chapter3/ref_ref.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>演算子</title>
+</head>
+
+<body>
+	<?php
+	$x = 1;
+	$y = &$x;
+	$x = 5;
+	print $y;
+	?>
+</body>
+
+</html>
