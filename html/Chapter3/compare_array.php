6b8875b813601006cd58cdf83c4efd251615eb0e
Soh1121 <satou.shg@gmail.com>
2021-11-02 11:18:41 +0900

    [add]配列の比較を学習

---
+++ html/Chapter3/compare_array.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>演算子</title>
+</head>
+
+<body>
+	<?php
+	$data01 = [1, 2, 3];
+	$data02 = [1, 5];
+	var_dump($data01 < $data02);
+
+	$data11 = [1, 2, 3];
+	$data12 = [1, 5, 1];
+	var_dump($data11 < $data12);
+
+	$data21 = [1, 2, 3];
+	$data22 = [1, 2, '3'];
+	var_dump($data21 == $data22);
+	var_dump($data21 === $data22);
+
+	$data31 = ['A' => 'a', 'B' => 'b', 'C' => 'c'];
+	$data32 = ['A' => 'a', 'C' => 'c', 'B' => 'b'];
+	var_dump($data31 == $data32);
+	var_dump($data31 === $data32);
+	?>
+</body>
+
+</html>
