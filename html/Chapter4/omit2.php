d6e9863d53b52273c95e40b4439fe98941c1f288
Soh1121 <satou.shg@gmail.com>
2021-11-03 11:04:18 +0900

    [add]中括弧は省略可能であることを学習

---
+++ html/Chapter4/omit2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>制御構文</title>
+</head>
+
+<body>
+	<?php
+	$x = 1;
+	$y = 0;
+	if ($x === 1) {
+		if ($y === 1) {
+			print '変数$x、$yは1です。';
+		}
+	} else {
+		print '変数$xは1ではありません。';
+	}
+	?>
+</body>
+
+</html>
