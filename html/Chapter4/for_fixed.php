4c3d72b826df6f29f9aeaa5d9ef8cf70beda06de
Soh1121 <satou.shg@gmail.com>
2021-11-03 20:20:15 +0900

    [add]スクリプトブロックと固定テンプレートの学習

---
+++ html/Chapter4/for_fixed.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>制御構文</title>
+</head>
+
+<body>
+	<?php for ($i = 1; $i < 6; $i++) { ?>
+		<p>こんにちは、世界！</p>
+	<?php } ?>
+</body>
+
+</html>
