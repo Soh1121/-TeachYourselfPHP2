af88c93885412a6c967b3a0ecbc0290dd86e1d70
Soh1121 <satou.shg@gmail.com>
2021-11-03 12:19:43 +0900

    [add]continue命令を学習

---
+++ html/Chapter4/continue.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>制御構文</title>
+</head>
+
+<body>
+	<?php
+	$sum = 0;
+	for ($i = 1; $i <= 100; $i++) {
+		if ($i % 2 !== 0) {
+			continue;
+		}
+		$sum += $i;
+	}
+	print "合計値は{$sum}です。";
+	?>
+</body>
+
+</html>
