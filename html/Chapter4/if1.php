60a8b5c37ecea8f42b894daa64ef9ff6e38835b2
Soh1121 <satou.shg@gmail.com>
2021-11-03 10:51:41 +0900

    [add]if命令の単純分岐を学習

---
+++ html/Chapter4/if1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>制御構文</title>
+</head>
+
+<body>
+	<?php
+	$x = 10;
+	if ($x === 10) {
+		print '変数$xは10です。';
+	} else {
+		print '変数$xは10ではありません。';
+	}
+	?>
+</body>
+
+</html>
---
+++ html/Chapter4/if2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>制御構文</title>
+</head>
+
+<body>
+	<?php
+	$x = 10;
+	if ($x === 10) {
+		print '変数$xは10です。';
+	}
+	?>
+</body>
+
+</html>
