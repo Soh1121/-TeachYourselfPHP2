3dd3b0ba1600164e82f6d225069b2ffa92b2e32d
Soh1121 <satou.shg@gmail.com>
2021-11-03 11:40:03 +0900

    [add]for命令を学習

---
+++ html/Chapter4/for.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>制御構文</title>
+</head>
+
+<body>
+	<?php
+	for ($i = 1; $i < 6; $i++) {
+		print "{$i}番目のループです。<br />";
+	}
+	?>
+</body>
+
+</html>
