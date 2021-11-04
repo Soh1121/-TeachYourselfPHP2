176e942e98e347687b4ebc7341be64353814753d
Soh1121 <satou.shg@gmail.com>
2021-11-04 14:55:16 +0900

    [add]count関数を学習
    配列の要素数を取得するため

---
+++ html/Chapter5/count.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>組み込み関数</title>
+</head>
+
+<body>
+	<?php
+	$data = ['山田', '掛谷', '日尾', '本多', '矢吹'];
+	print count($data);
+	?>
+</body>
+
+</html>
