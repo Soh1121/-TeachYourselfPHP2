6f84fbba8dee8cd960199244ddbbbafe2259d914
Soh1121 <satou.shg@gmail.com>
2021-11-04 15:04:13 +0900

    [add]implode関数を学習
    配列の各要素を結合するため

---
+++ html/Chapter5/implode.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>組み込み関数</title>
+</head>
+
+<body>
+	<?php
+	$data = ['PHP', 'Perl', 'Ruby', 'Python', 'JavaScript'];
+	print implode(',', $data);
+	?>
+</body>
+
+</html>
