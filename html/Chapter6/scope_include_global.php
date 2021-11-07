4bd19b7448262884b75ee1aa3a310eccb53bb3bb
Soh1121 <satou.shg@gmail.com>
2021-11-07 21:16:16 +0900

    [add]インクルードファイルのスコープ

---
+++ html/Chapter6/scope_include_global.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>ユーザ定義関数</title>
+</head>
+
+<body>
+	<?php
+	require_once 'scope_included.php';
+
+	print $scope;
+	?>
+</body>
+
+</html>
