b843c1dc752d9de8c91dcd429ad7b446942d62c3
Soh1121 <satou.shg@gmail.com>
2021-11-15 18:18:03 +0900

    [add]DirectoryIteratorクラスを学習

---
+++ html/Chapter7/dir.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>標準クラスライブラリ</title>
+</head>
+
+<body>
+	<table border="1">
+		<tr>
+			<th>ファイル</th>
+			<th>サイズ</th>
+			<th>最終アクセス日</th>
+			<th>最終更新日</th>
+		</tr>
+		<?php
+		// カレントフォルダをオープン
+		$dir = new DirectoryIterator('./');
+		// フォルダの内容を順番に読み込み
+		foreach ($dir as $file) {
+			// 読み込んだ要素がファイルである（サブフォルダなどでない）場合のみ表示処理
+			if ($file->isFile()) {
+		?>
+				<tr>
+					<td><?php print mb_convert_encoding($file->getFileName(), 'UTF-8', 'SJIS-WIN'); ?></td>
+					<td><?php print $file->getSize(); ?>B</td>
+					<td><?php print date('Y/m/d H:i:s', $file->getATime()); ?></td>
+					<td><?php print date('Y/m/d H:i:s', $file->getMTime()); ?></td>
+				</tr>
+		<?php
+			}
+		}
+		?>
+	</table>
+</body>
+
+</html>
