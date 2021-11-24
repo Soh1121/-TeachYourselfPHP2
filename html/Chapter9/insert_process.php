f2039fdb9549a5a1c227851ae2e9a1649f201bec
Soh1121 <satou.shg@gmail.com>
2021-11-24 11:55:01 +0900

    [update]名前付きパラメータと名前なしパラメータを学習

--- html/Chapter9/insert_process.php
+++ html/Chapter9/insert_process.php
@@ -5,13 +5,13 @@ try {
 	// データベースへの接続を確立
 	$db = getDb();
 	// INSERT命令の準備
-	$stt = $db->prepare('INSERT INTO book(isbn, title, price, publish, published) VALUES(:isbn, :title, :price, :publish, :published)');
+	$stt = $db->prepare('INSERT INTO book(isbn, title, price, publish, published) VALUES(?, ?, ?, ?, ?)');
 	// INSERT命令にポストデータの内容をセット
-	$stt->bindValue(':isbn', $_POST['isbn']);
-	$stt->bindValue(':title', $_POST['title']);
-	$stt->bindValue(':price', $_POST['price']);
-	$stt->bindValue(':publish', $_POST['publish']);
-	$stt->bindValue(':published', $_POST['published']);
+	$stt->bindValue(1, $_POST['isbn']);
+	$stt->bindValue(2, $_POST['title']);
+	$stt->bindValue(3, $_POST['price']);
+	$stt->bindValue(4, $_POST['publish']);
+	$stt->bindValue(5, $_POST['published']);
 	// INSERT命令を実行
 	$stt->execute();
 	// 処理後は入力フォームにリダイレクト
