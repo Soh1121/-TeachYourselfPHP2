6522a5b09d313a32221b40c7d4523024bac97814
Soh1121 <satou.shg@gmail.com>
2021-11-26 15:21:43 +0900

    [update]final修飾子を学習
    オーバライドを禁止するため

--- html/Chapter10/BusinessPerson.php
+++ html/Chapter10/BusinessPerson.php
@@ -2,10 +2,10 @@
 // 継承元のクラスファイルをインポート
 require_once 'Person4.php';
 
-class BusinessPerson extends Person
+final class BusinessPerson extends Person
 {
 	// サブクラス独自のworkメソッドを定義
-	public function work()
+	public final function work()
 	{
 		print "<p>{$this->lastName}{$this->firstName}は働いています。</p>";
 	}
